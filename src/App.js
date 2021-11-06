63b10ea10b1de9eaed9d1294eeed7cbbd96765ad
jose armando <jose4armando@outlook.com>
2021-11-05 19:31:48 -0600

    fixing bugs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { faLaughWink } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
-import { useState } from 'react/cjs/react.development';
+import React, { useState } from 'react';
 import './App.css';
 import  ButtonCreate from './components/ButtonCreate';
 import Card from "./components/Card";
